c66c3b3badba999f17ceeaeaef4e2d887968bb82
stanken_a <artyemst3@mail.ru>
2023-08-10 00:32:28 +0300

    feat: add swipers

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,43 @@
+const heroSwiper = new Swiper('.hero__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".hero__swiper-pagination",
+    clickable: true,
+  },
+});
+
+const reasonFirstSwiper = new Swiper('.reason__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".reason__swiper-pagination",
+    clickable: true,
+  },
+});
+
+const reasonSecondSwiper = new Swiper('.reason__swiper-small', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".reason__swiper-small-pagination",
+    clickable: true,
+  },
+});
+
+const feedbackSwiper = new Swiper('.feedback__swiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".feedback__swiper-pagintaion",
+    clickable: true,
+  },
+});
+
 class Header {
   constructor(headerSelector) {
     this._header = document.querySelector(headerSelector);
@@ -25,5 +65,5 @@ class Header {
   }
 }
 
-const header = new Header('.header');
-header.setEventListeners();
+// const header = new Header('.header');
+// header.setEventListeners();
